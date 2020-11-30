2825fbbe37293730772bdf9e8be1043216197690
Karina Gordienko <aiwpront@gmail.com>
2020-11-30 19:05:24 +0400

    Рыба первого теста + создание файла POM

---
+++ src/test/java/tests/googleCalcTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class googleCalcTests {
+
+    @Test
+    public void test1(){
+        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver.get("https://www.google.ru/");
+        driver.quit();
+    }
+
+}
