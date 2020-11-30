0c9b5b1b53aa8289e9f0087a10ca0882ada163fc
Karina Gordienko <aiwpront@gmail.com>
2020-11-30 19:29:23 +0400

    Написаны все необходимые @Findby в POM для выполнения трёх практик

--- src/test/java/tests/googleCalcTests.java
+++ src/test/java/tests/googleCalcTests.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class googleCalcTests {
@@ -27,24 +26,31 @@ public class googleCalcTests {
     public void test1() {
         driver.get("https://www.google.ru/");
         driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
+        //(
         driver.findElement(By.cssSelector("div[aria-label=\"открывающая скобка\"]")).click();
         //1
         driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"N10B9\"]")).click();
+        //+
         driver.findElement(By.cssSelector("div[aria-label=\"сложение\"]")).click();
         //2
         driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"lVjWed\"]")).click();
+        //)
         driver.findElement(By.cssSelector("div[aria-label=\"закрывающая скобка\"]")).click();
+        //*
         driver.findElement(By.cssSelector("div[aria-label=\"умножение\"]")).click();
         //3
         driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"KN1kY\"]")).click();
+        //-
         driver.findElement(By.cssSelector("div[aria-label=\"вычитание\"]")).click();
         //4
         driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"xAP7E\"]")).click();
         //0
         driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"bkEvMb\"]")).click();
+        //:
         driver.findElement(By.cssSelector("div[aria-label=\"деление\"]")).click();
         //5
         driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"Ax5wH\"]")).click();
+        //=
         driver.findElement(By.cssSelector("div[aria-label=\"равно\"]")).click();
         assertEquals("(1 + 2) × 3 - 40 ÷ 5 =",driver.findElement(By.cssSelector("div [jsname=\"VkJw6\"] span")).getText());
         assertEquals("1",driver.findElement(By.cssSelector("div [jsname=\"zLiRgc\"] span")).getText());
